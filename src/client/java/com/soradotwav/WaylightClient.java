5c6e045e2826a20c2e0bdb61fc2378bcd4447919
soradotwav <dev@soradotwav.com>
2026-03-09 19:43:26 -0700

    Initial commit

---
+++ src/client/java/com/soradotwav/WaylightClient.java
@@ -0,0 +1,10 @@
+package com.soradotwav;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WaylightClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
